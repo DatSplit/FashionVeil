4d732efb889b0c3d2905417d0af3a26cd9dde071
DatSplit <nielsteunissen@hotmail.nl>
2025-08-26 16:57:39 +0200

    Checked fashionfail changes.

--- fashionfail/src/fashionfail/models/predict_rtdetr.py
+++ fashionfail/src/fashionfail/models/predict_rtdetr.py
@@ -53,46 +53,35 @@ def predict_with_onnx(model_name, image_dir, out_dir, model_path):
         providers=['CUDAExecutionProvider', 'CPUExecutionProvider']
     )
 
-    # Load and initialize the feature extractor with specific size
     feature_extractor = RTDetrImageProcessor.from_pretrained(
         "fashionfail/models/configs",
-        size={"height": 800, "width": 800},  # Match export dimensions
-        # do_resize=True,
-        # do_normalize=True,
-        # do_rescale=True,
-        # do_pad=True,
+        size={"height": 800, "width": 800},
         max_size=800
     )
 
-    # Run inference on images, accumulate results in a list, save as `.npz` file.
     preds = []
     proba_threshold = 0.5
 
     logger.debug("Running inference now...")
     for image_path in tqdm(glob(os.path.join(image_dir, "*.jpg"))):
-        # Open image
         image = Image.open(image_path)
-        # Convert to RGB if needed
+
         if image.mode != 'RGB':
             image = image.convert('RGB')
 
-        # Process image with feature extractor
         inputs = feature_extractor(
             images=image,
             return_tensors="pt",
         )
         pixel_values = inputs.pixel_values
 
-        # Run inference
         ort_inputs = {
             'pixel_values': pixel_values.numpy()
         }
 
         logits, pred_boxes = session.run(['logits', 'pred_boxes'], ort_inputs)
 
-        # Post-process the outputs
         scores = torch.sigmoid(torch.from_numpy(logits))
-        # Get predictions above threshold
         max_scores, pred_labels = scores.max(-1)
         mask = max_scores > proba_threshold
         pred_boxes_abs = convert_to_absolute_coordinates(
@@ -103,8 +92,6 @@ def predict_with_onnx(model_name, image_dir, out_dir, model_path):
         )
         filtered_scores = max_scores[mask]
         filtered_labels = pred_labels[mask]
-
-        # Accumulate results.
         preds.append(
             {
                 "image_file": Path(image_path).name,
@@ -113,7 +100,6 @@ def predict_with_onnx(model_name, image_dir, out_dir, model_path):
                 "scores": filtered_scores.numpy(),
             }
         )
-    # Save results in a compressed `.npz` file
     os.makedirs(out_dir, exist_ok=True)
     out_file_name = model_name + ".npz"
     np.savez_compressed(os.path.join(out_dir, out_file_name), data=preds)
@@ -121,10 +107,8 @@ def predict_with_onnx(model_name, image_dir, out_dir, model_path):
 
 
 if __name__ == "__main__":
-    # Parse args
+
     parser = get_cli_args_parser()
     args = parser.parse_args()
-
-    # call the respective function
     predict_with_onnx(args.model_name, args.image_dir,
                       args.out_dir, args.model_path)
